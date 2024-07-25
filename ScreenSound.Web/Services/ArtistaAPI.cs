80f3e9a2c3f1c50d06bc141d81496374c6981b4e
Yuri-Almir-Pinto <yurialmirp@gmail.com>
2024-07-25 09:31:24 -0300

    Adicionar "ArtistaAPI"
    Adicionado a classe "ArtistaAPI" para conter os métodos para acessar os diferentes endpoints da API de maneira programática.
    
    Também adicionado as classes contendo os DTOs que serão utilizados pela "ArtistaAPI".

---
+++ ScreenSound.Web/Services/ArtistaAPI.cs
@@ -0,0 +1,15 @@
+﻿using ScreenSound.Web.Response;
+using System.Net.Http.Json;
+
+namespace ScreenSound.Web.Services;
+
+public class ArtistaAPI(IHttpClientFactory httpClient)
+{
+    public const string HttpClientName = "ArtistaAPI";
+    private readonly HttpClient _httpClient = httpClient.CreateClient(HttpClientName);
+
+    public async Task<ICollection<ArtistaResponse>?> GetArtistasAsync()
+    {
+        return await _httpClient.GetFromJsonAsync<ICollection<ArtistaResponse>>("artistas");
+    }
+}
