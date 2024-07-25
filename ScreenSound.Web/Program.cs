9af746c298dd6e7a63739c7891b30a1a061257b5
Yuri-Almir-Pinto <yurialmirp@gmail.com>
2024-07-25 09:34:19 -0300

    Configurar "ArtistasAPI"
    Configurado o projeto para utilizar da classe "ArtistasAPI", a adicionando como um recurso transiente na aplicação.
    
    Também adicionado um novo arquivo no wwwroot, chamado "AppSettings", para conter algumas configurações da API, incluindo a URL da API.

--- ScreenSound.Web/Program.cs
+++ ScreenSound.Web/Program.cs
@@ -1,11 +1,18 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ScreenSound.Web;
+using ScreenSound.Web.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddTransient<ArtistaAPI>();
+
+builder.Services.AddHttpClient(ArtistaAPI.HttpClientName, c =>
+{
+    c.BaseAddress = new Uri(builder.Configuration["APIServer:Url"]!);
+    c.DefaultRequestHeaders.Add("Accept", "application/json");
+});
 
 await builder.Build().RunAsync();
